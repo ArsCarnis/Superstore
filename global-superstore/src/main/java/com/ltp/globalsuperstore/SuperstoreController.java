a1c0c3d11c39bbdf25abe782f431f72745843395
MT <matchan@vivaldi.net>
2022-07-22 23:43:55 +0200

    Added input validation mechanism.

--- global-superstore/src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
@@ -2,11 +2,14 @@ package com.ltp.globalsuperstore;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,15 +23,17 @@ public class SuperstoreController {
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getItemIndex(id);
-        model.addAttribute("categories", Constants.CATEGORIES);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : itemsList.get(index));
         return "form";
     }
 
 
     @PostMapping("/submitItem")
-    public String submitForm(Item item, RedirectAttributes redirectAttributes) {
+    public String submitForm(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
         String status = Constants.STATUS_SUCCESS;
+        if (item.getPrice()<item.getDiscount()) result.rejectValue("price","","Price cannot be less than discount");
+        if (result.hasErrors()) return "form";
+
         int index = getItemIndex(item.getId());
         if (index == Constants.NOT_FOUND) {
             itemsList.add(item);
